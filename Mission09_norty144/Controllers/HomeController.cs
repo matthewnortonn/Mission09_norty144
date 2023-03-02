0baea66ec248439e6bef2f7c4b664e2dec2a2ef4
Matthew Norton <matthew.lee.norton@gmail.com>
2023-03-01 22:31:32 -0700

    Add project files.

---
+++ Mission09_norty144/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_norty144.Models;
+using Mission09_norty144.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_norty144.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Author)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+    }
+}
---
+++ Mission09_norty144/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_norty144.Models.ViewModels
+{
+    public class PageInfo
+    {
+        //these will generate the total number of books, books per page, and the current page info for the visitor
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        //figures out how many pages we need
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumBooks / BooksPerPage);
+    }
+}
